fdd3bc3396a4f683be5565216fd0cf69b83b412a
christiansonza <103972546+christiansonza@users.noreply.github.com>
2026-03-05 15:49:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(morgan('dev'))
 app.use(helmet())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://sysTestVercel.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
 }));
