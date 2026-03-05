5f8e8c2894579f1587135efc3344c4ea391c4c44
christiansonza <103972546+christiansonza@users.noreply.github.com>
2026-03-05 16:54:48 +0800

    Remove AuthMiddleware from user routes
    Removed AuthMiddleware from routes for getting current user, getting user by ID, and updating user.

--- router/userRouter.js
+++ router/userRouter.js
@@ -7,18 +7,18 @@ import {
   updateUser,
   getCurrentUser
 } from '../controller/userController.js';
-import AuthMiddleware from '../middleware/authMiddleware.js';
+
 
 const router = express.Router();
 
 
-router.get('/loggedUser', AuthMiddleware, getCurrentUser);
+router.get('/loggedUser', getCurrentUser);
 
-router.get('/', AuthMiddleware, getUser);
-router.get('/:id', AuthMiddleware, getUserId);
+router.get('/', getUser);
+router.get('/:id', getUserId);
 router.post('/register', createUser);
 router.post('/login', logUser);
-router.put('/:id', AuthMiddleware, updateUser);
+router.put('/:id', updateUser);
 
 
 router.post('/logout', (req, res) => {
